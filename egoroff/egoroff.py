f379b19fd7bdedfa98d1c7fc34d6049ddbdf685e
voidrider999 <voidrider999@gmail.com>
2024-06-17 20:59:11 +0500

    Add egoroff

---
+++ egoroff/egoroff.py
@@ -0,0 +1,26 @@
+print('hello')
+print('''hello
+world''')
+hello = input()
+hello2 = 'hello'
+d = ''
+e = '  '
+abc = 'abc' + 'dec'
+print(abc)
+s = 'hello'
+c = 'world'
+u = s+c
+print(u)
+u = s+'  '+c
+print(u)
+print(u * 5)
+print('abc' + str(1))
+print(len('abc'))
+print(len(''))
+print(len(' '))
+r = '56789034'
+if '4' in r:
+    print('True')
+print(ord('4'))
+print(ord('ы'))
+print('vi' in 'provider')
