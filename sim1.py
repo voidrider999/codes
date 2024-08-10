17af7988c48f607f22399ce93eb3dd3ed8fc2c43
voidrider999 <voidrider999@gmail.com>
2024-08-10 20:47:22 +0500

    Update sim1.py

--- sim1.py
+++ sim1.py
@@ -11,41 +11,35 @@ space = pymunk.Space()
 space.gravity = (0, 1000)
 draw_opts = pymunk.pygame_util.DrawOptions(screen)
 
-for i in range(10):
-    x = random.randint(0, 500)
-    r = random.randint(10, 20)
-    body = pymunk.Body()
-    body.position = (x, 0)
-    shape = pymunk.Circle(body, r)
-    shape.mass = 1
-    shape.elasticity = 0.7
-    shape.friction = 1
-    space.add(body, shape)
 
-line1 = pymunk.Segment(space.static_body, (50, 100), (200, 200), 2)
-line1.elasticity = 0.7
+body = pymunk.Body()
+body.position = (5, 0)
+shape = pymunk.Circle(body, 10)
+shape.mass = 1
+shape.elasticity = 1
+shape.friction = 1
+space.add(body, shape)
+
+line1 = pymunk.Segment(space.static_body, (0, 0), (140, 480), 2)
+line1.elasticity = 1
 line1.friction = 1
 space.add(line1)
 
-line2 = pymunk.Segment(space.static_body, (230, 300), (500, 100), 2)
-line2.elasticity = 0.7
+line2 = pymunk.Segment(space.static_body, (140, 480), (180, 485), 2)
+line2.elasticity = 2
 line2.friction = 1
 space.add(line2)
 
-line3 = pymunk.Segment(space.static_body, (0, 500), (500, 400), 2)
-line2.elasticity = 0.7
+line3 = pymunk.Segment(space.static_body, (200, 400), (300, 405), 2)
+line2.elasticity = 0.6
 line2.friction = 1
 space.add(line3)
 
-line4 = pymunk.Segment(space.static_body, (0, 300), (200, 400), 2)
-line4.elasticity = 0.7
+line4 = pymunk.Segment(space.static_body, (300, 500), (360, 500), 2)
+line4.elasticity = 2
 line4.friction = 1
 space.add(line4)
-
-stop_line1 = pymunk.Segment(space.static_body, (0, 500), (0, 450), 2)
-stop_line1.elasticity = 0.7
-stop_line1.friction = 1
-space.add(stop_line1)
+i = 0
 
 while True:
     space.step(1/60)
@@ -53,3 +47,13 @@ while True:
     space.debug_draw(draw_opts)
     pygame.display.flip()
     clock.tick(60)
+    i += 1
+    if i % 15 == 0:
+        body = pymunk.Body()                     
+        body.position = (5, 0)
+        shape = pymunk.Circle(body, 10)
+        shape.mass = 1          
+        shape.elasticity = 1                    
+        shape.friction = 1                      
+        space.add(body, shape)
+    
