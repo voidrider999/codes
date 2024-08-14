90c3c69404d9999555d8f809668e871484b55653
voidrider999 <voidrider999@gmail.com>
2024-08-14 18:03:48 +0500

    Revert "Fix sim1.py"
    This reverts commit 76c957a38e4d8fab27b02e5c1e97c99b6ab499c4.

--- sim1.py
+++ sim1.py
@@ -11,23 +11,23 @@ space = pymunk.Space()
 space.gravity = (0, 1000)
 draw_opts = pymunk.pygame_util.DrawOptions(screen)
 
-line1 = pymunk.Segment(space.static_body, (200, 0), (500, 0), 2)
-line1.elasticity = 0.5
+line1 = pymunk.Segment(space.static_body, (0, 0), (140, 480), 2)
+line1.elasticity = 1
 line1.friction = 1
 space.add(line1)
 
-line2 = pymunk.Segment(space.static_body, (100, 450), (500, 400), 2)
-line2.elasticity = 0.5
+line2 = pymunk.Segment(space.static_body, (140, 480), (180, 485), 2)
+line2.elasticity = 2
 line2.friction = 1
 space.add(line2)
 
-line3 = pymunk.Segment(space.static_body, (0, 100), (0, 500), 2)
-line2.elasticity = 0.5
+line3 = pymunk.Segment(space.static_body, (200, 400), (300, 405), 2)
+line2.elasticity = 0.6
 line2.friction = 1
 space.add(line3)
 
-line4 = pymunk.Segment(space.static_body, (0, 100), (200, 0), 2)
-line4.elasticity = 0.5
+line4 = pymunk.Segment(space.static_body, (300, 500), (450, 500), 2)
+line4.elasticity = 2
 line4.friction = 1
 space.add(line4)
 
@@ -35,11 +35,11 @@ i = 0
 while True:
     if i % 15 == 0:
         body = pymunk.Body()
-        body.position = (400, 200)
+        body.position = (5, 0)     
         shape = pymunk.Circle(body, 10)
-        shape.mass = 1
+        shape.mass = 1            
         shape.elasticity = 1
-        shape.friction = 1
+        shape.friction = 1       
         space.add(body, shape)
 
     space.step(1/60)
