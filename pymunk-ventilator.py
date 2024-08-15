d0d073e7d6e60ffa3338b0b1dc7a582552938876
voidrider999 <voidrider999@gmail.com>
2024-08-15 12:59:03 +0500

    Update pymunk-ventilator.py

--- pymunk-ventilator.py
+++ pymunk-ventilator.py
@@ -13,43 +13,41 @@ draw_opts = pymunk.pygame_util.DrawOptions(screen)
 
 line1 = pymunk.Segment(space.static_body, (200, 0), (500, 0), 5)
 line1.elasticity = 0.5
-line1.friction = 1
+line1.friction = 0.1
 space.add(line1)
 
 line2 = pymunk.Segment(space.static_body, (100, 450), (500, 400), 5)
 line2.elasticity = 0.5
-line2.friction = 1
+line2.friction = 0.1
 space.add(line2)
 
 line3 = pymunk.Segment(space.static_body, (0, 100), (0, 500), 5)
 line3.elasticity = 0.5
-line3.friction = 1
+line3.friction = 0.1
 space.add(line3)
 
 line4 = pymunk.Segment(space.static_body, (0, 100), (200, 0), 5)
 line4.elasticity = 0.5
-line4.friction = 1
+line4.friction = 0.1
 space.add(line4)
 
 line5 = pymunk.Segment(space.static_body, (500, 0), (500, 400), 5)
 line5.elasticity = 0.5
-line5.friction = 1
+line5.friction = 0.1
 space.add(line5)
 
-i = 0
-while True:
-    if i % 15 == 0:
-        body = pymunk.Body()
-        body.position = (400, 200)
-        shape = pymunk.Circle(body, 10)
-        shape.mass = 1
-        shape.elasticity = 1
-        shape.friction = 1
-        space.add(body, shape)
+for i in range(20):
+    body = pymunk.Body()
+    body.position = (400, 50)
+    shape = pymunk.Circle(body, 20)
+    shape.mass = 1
+    shape.elasticity = 0.5
+    shape.friction = 0.1
+    space.add(body, shape)
 
+while True:
     space.step(1/60)
     screen.fill((0, 0, 0))
     space.debug_draw(draw_opts)
     pygame.display.flip()
     clock.tick(60)
-    i += 1
