aed684a2f9e2912174c5189ece884f2ce311a43b
voidrider999 <voidrider999@gmail.com>
2024-08-17 15:10:51 +0500

    Fix lesson14-list-methods.py

--- egoroff/lesson14-list-methods.py
+++ egoroff/lesson14-list-methods.py
@@ -1,19 +1,19 @@
 a = [12, 43, 51]
-a.append(16) # 12, 43, 51, 16
-print(a)
+a.append(16)
+print(a) # 12, 43, 51, 16
 a.clear()
-print(a)
+print(a) # []
 a = [1, 2, 3]
-b = a.copy() # 1, 2, 3
-print(b)
-b = a[:] # 1, 2, 3
-print(b)
-b.append(12) # 1, 2, 3, 12
-b.append(12) # 1, 2, 3, 12, 12
-print(b)
-print(b.count(12)) # 2
-print(b.index(12, 3)) # 3, первый параметр - что искать, второй - с какого индекса искать
-print(b.index(12, 3, 5)) # 3, третий параметр - каким индексом закончить поиск
+b = a.copy()
+print(b) # 1, 2, 3
+b = a[:]
+print(b) # 1, 2, 3
+b.append(12)
+b.append(12) 
+print(b) # 1, 2, 3, 12
+print(b.count(12))
+print(b.index(12, 3))
+print(b.index(12, 3, 5))
 c = 12 in b[3:5]
 print(c) # True
 c = b.insert(0,1)
