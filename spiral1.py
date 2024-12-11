375fcb59d7057b0be3ffd3405a91eca7a5fe9bc4
voidrider999 <voidrider999@gmail.com>
2024-12-11 14:18:27 +0200

    Add spiral1.py and spiral2.py

---
+++ spiral1.py
@@ -0,0 +1,7 @@
+from turtle import *
+width(5)
+speed(10)
+for i in range(21):
+    fd(i*5)
+    rt(90)
+input()
---
+++ spiral2.py
@@ -0,0 +1,8 @@
+from turtle import *
+width(5)
+speed(10)
+for i in range(4):
+    for i in range(21):
+        fd(i*5)
+        rt(90)
+input()
