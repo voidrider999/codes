d13ba09c341c244237d58d4ae5b2f76f8aa25f5d
voidrider999 <voidrider999@gmail.com>
2024-12-16 16:33:04 +0200

    Update spiral1.py

--- spiral1.py
+++ spiral1.py
@@ -1,7 +1,14 @@
 from turtle import *
+import random as r
 width(5)
 speed(10)
-for i in range(21):
-    fd(i*5)
-    rt(90)
+for i in range(100000000000000):
+    dist = i * 5 / 3 
+    color('blue')
+    fd(dist)
+    color('red')
+    fd(dist)
+    color('green')
+    fd(dist)
+    rt(89)
 input()
