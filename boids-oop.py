705728ac5ebeb9af803c5c9de03a174c60951303
voidrider999 <voidrider999@gmail.com>
2025-01-27 13:22:29 +0200

    Add boids-oop.py

---
+++ boids-oop.py
@@ -0,0 +1,98 @@
+import pygame
+import random
+
+N = 50
+VX_MAX = 10
+VY_MAX = 10
+HEIGHT = 500
+WIDTH = 500
+PROTECTED = 17
+VISIBLE = 50
+PROTECTED_COEF = 0.05
+SYNC_COEF = 0.3
+CENTER_COEF = 0.01
+WALL_COEF = 2
+
+pygame.init()
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+clock = pygame.time.Clock()
+
+class Boid:
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
+boids = []
+for _ in range(N):
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    vx = random.randint(-VX_MAX, VX_MAX)
+    vy = random.randint(-VY_MAX, VY_MAX)
+    boid = Boid(x, y, vx, vy)
+    boids.append(boid)
+
+while True:
+    screen.fill((0, 0, 0))
+    for i in range(len(boids)):
+        boid = boids[i]
+        visible_avg_vx = 0
+        visible_avg_vy = 0
+        visible_avg_x = 0
+        visible_avg_y = 0
+        visible_count = 0
+        for j in range(len(boids)):
+            if i == j:
+                continue
+            other = boids[j]
+            dist_x = boid.x - other.x
+            dist_y = boid.y - other.y
+            if abs(dist_x) <= PROTECTED and abs(dist_y) <= PROTECTED:
+                boid.vx += dist_x * PROTECTED_COEF
+                boid.vy += dist_y * PROTECTED_COEF
+            elif abs(dist_x) <= VISIBLE and abs(dist_y) <= VISIBLE:
+                visible_count += 1
+                visible_avg_vx += other.vx
+                visible_avg_vy += other.vy
+                visible_avg_x += other.x
+                visible_avg_y += other.y
+            else:
+                pass
+
+        if visible_count > 0:
+            visible_avg_vx /= visible_count
+            visible_avg_vy /= visible_count
+            boid.vx += (visible_avg_vx - boid.vx) * SYNC_COEF
+            boid.vy += (visible_avg_vy - boid.vy) * SYNC_COEF
+
+            visible_avg_x /= visible_count
+            visible_avg_y /= visible_count
+            dist_x = boid.x - visible_avg_x
+            dist_y = boid.y - visible_avg_y
+            boid.vx += -dist_x * CENTER_COEF
+            boid.vy += -dist_y * CENTER_COEF
+
+        if boid.x >= WIDTH - 50:
+            boid.vx -= WALL_COEF
+        if boid.x <= 50:
+            boid.vx += WALL_COEF
+        if boid.y >= HEIGHT - 50:
+            boid.vy -= WALL_COEF
+        if boid.y <= 50:
+            boid.vy += WALL_COEF
+
+        if boid.vx > VX_MAX:
+            boid.vx = VX_MAX
+        if boid.vx < -VX_MAX:
+            boid.vx = -VX_MAX
+        if boid.vy > VY_MAX:
+            boid.vy = VY_MAX
+        if boid.vy < -VY_MAX:
+            boid.vy = -VY_MAX
+
+        boid.x += boid.vx
+        boid.y += boid.vy
+        pygame.draw.circle(screen, (255, 0, 0), (boid.x, boid.y), 5)
+    pygame.display.flip()
+    clock.tick(60)
