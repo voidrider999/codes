d33031a359f1819d3bc0911dd3ed1cf6902a3a70
voidrider999 <voidrider999@gmail.com>
2025-02-21 12:43:14 +0200

    snake: movement in all directions

--- snake.py
+++ snake.py
@@ -11,9 +11,29 @@ snake = [
     {'xc': 26, 'yc': 25},
 ]
 
-speed = 1 # cells/sec
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
+speed = 4 # cells/sec
 dist = 0
-while True:
+direction = RIGHT
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                direction = LEFT
+            elif event.key == pygame.K_RIGHT:
+                direction = RIGHT 
+            elif event.key == pygame.K_UP:
+                direction = UP
+            elif event.key == pygame.K_DOWN:
+                direction = DOWN
+
     t = clock.tick(10) / 1000
     dist += t * speed
     if dist >= 1:
@@ -25,7 +45,14 @@ while True:
             cell['yc'] = nxt['yc']
         
         head = snake[-1]
-        head['xc'] += 1
+        if direction == RIGHT:
+            head['xc'] += 1
+        elif direction == LEFT:
+            head['xc'] -= 1
+        elif direction == UP:
+            head['yc'] -= 1
+        elif direction == DOWN:
+            head['yc'] += 1
 
     print(t, dist)
     screen.fill((0, 0, 0))
@@ -35,6 +62,6 @@ while True:
         if i % 2 == 0:
             color = (255, 0, 0)
         else:
-            color = (0, 0, 255)
+            color = (0, 255, 0)
         pygame.draw.rect(screen, color, pygame.Rect(x, y, 10, 10))
     pygame.display.flip()
