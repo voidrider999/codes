fde2b4c7adf3f888967268d3c542fd275a9e3f42
voidrider999 <voidrider999@gmail.com>
2025-05-09 16:39:38 +0200

    turtle-tk: автоизменение размеров для виджетов и канваса

--- turtle-tk.py
+++ turtle-tk.py
@@ -1,19 +1,19 @@
 # TODO return to canvas when moved out of it
 # TODO color picker (fg, bg)
 # TODO turn turtle in real time
-# TODO canvas to the right of widgets
 
 import tkinter as tk
 from tkinter import ttk
 from turtle import RawTurtle
 
 root = tk.Tk()
-root.geometry("250x500")
 root.eval('tk::PlaceWindow . center')
 
-label = ttk.Label(text='Длина')
+frame = ttk.Frame(padding=(2, 2))
+
+label = ttk.Label(frame, text='Длина')
 label.pack()
-length_sb = ttk.Spinbox(from_=1, to=100, increment=1)
+length_sb = ttk.Spinbox(frame, from_=1, to=100, increment=1)
 length_sb.set(1)
 length_sb.pack()
 
@@ -22,9 +22,10 @@ def on_heading_validate():
     print('okay')
     return True
 
-label = ttk.Label(text='Поворот')
+label = ttk.Label(frame, text='Поворот')
 label.pack()
 heading_sb = ttk.Spinbox(
+    frame,
     from_=-360,
     to=360,
     increment=10,
@@ -34,21 +35,21 @@ heading_sb = ttk.Spinbox(
 heading_sb.set(0)
 heading_sb.pack()
 
-label = ttk.Label(text='Red')
+label = ttk.Label(frame, text='Red')
 label.pack()
-red_sb = ttk.Spinbox(from_=0, to=255, increment=1)
+red_sb = ttk.Spinbox(frame, from_=0, to=255, increment=1)
 red_sb.set(0)
 red_sb.pack()
 
-label = ttk.Label(text='Green')
+label = ttk.Label(frame, text='Green')
 label.pack()
-green_sb = ttk.Spinbox(from_=0, to=255, increment=1)
+green_sb = ttk.Spinbox(frame, from_=0, to=255, increment=1)
 green_sb.set(0)
 green_sb.pack()
 
-label = ttk.Label(text='Blue')
+label = ttk.Label(frame, text='Blue')
 label.pack()
-blue_sb = ttk.Spinbox(from_=0, to=255, increment=1)
+blue_sb = ttk.Spinbox(frame, from_=0, to=255, increment=1)
 blue_sb.set(0)
 blue_sb.pack()
 
@@ -61,13 +62,19 @@ def on_draw_click():
     ))
     turtle.forward(int(length_sb.get()))
 
-draw_btn = ttk.Button(text='Draw', command=on_draw_click)
+draw_btn = ttk.Button(frame, text='Draw', command=on_draw_click)
 draw_btn.pack()
 
-canvas = tk.Canvas(width=250, height=250)
-canvas.pack()
+canvas = tk.Canvas()
+
+# если не указать вес, то он не растянет ячейки грида по ширине и высоте
+# даже если ряд всего один, вес указывать нужно
+root.rowconfigure(index=0, weight=1)
+for c in range(2): root.columnconfigure(index=c, weight=1)
+frame.grid(row=0, column=0)
+# sticky=nswe растягивает виджет на всю ячейку (с севера на юг и с запада
+# на восток)
+canvas.grid(row=0, column=1, sticky='nswe')
 
 turtle = RawTurtle(canvas)
-print(turtle.xcor(), turtle.ycor())
-
 root.mainloop()
