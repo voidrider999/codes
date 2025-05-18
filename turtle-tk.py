dc32630c7a79ccc1756d0d46e3f5c72eee8b8695
voidrider999 <voidrider999@gmail.com>
2025-05-18 20:18:21 +0200

    turtle-tk: add image saving

--- turtle-tk.py
+++ turtle-tk.py
@@ -5,6 +5,9 @@
 import tkinter as tk
 from tkinter import ttk
 from turtle import RawTurtle
+from tkinter import filedialog
+from PIL import Image, ImageTk
+from io import BytesIO
 
 root = tk.Tk()
 root.eval('tk::PlaceWindow . center')
@@ -66,6 +69,15 @@ draw_btn = ttk.Button(frame, text='Draw', command=on_draw_click)
 draw_btn.pack()
 
 canvas = tk.Canvas()
+
+def save_image():
+    canvas.update()
+    eps = canvas.postscript(colormode='color')
+    im = Image.open(BytesIO(bytes(eps,'ascii')))
+    im.save('result.png')
+
+save_btn = ttk.Button(frame, text='Save', command=save_image)
+save_btn.pack()
 
 # если не указать вес, то он не растянет ячейки грида по ширине и высоте
 # даже если ряд всего один, вес указывать нужно
