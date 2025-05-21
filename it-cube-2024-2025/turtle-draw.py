48628d19bfea65530fa0b18046f6152787d4e4e1
voidrider999 <voidrider999@gmail.com>
2025-05-21 23:34:15 +0200

    turtle-draw: понятные имена виджетов и функций для режима повторов

--- it-cube-2024-2025/turtle-draw.py
+++ it-cube-2024-2025/turtle-draw.py
@@ -4,7 +4,7 @@ from turtle import RawTurtle
 from PIL import Image
 from io import BytesIO
 
-def draw_manual():
+def draw_manual_mode():
     turtle.getscreen().colormode(255)
     turtle.pencolor((red.get(), green.get(), blue.get()))
     turtle.setheading(heading.get())
@@ -13,7 +13,7 @@ def draw_manual():
     rgb = (red.get(), green.get(), blue.get())
     print(f'x:{x}, y:{y}, heading:{heading.get()}, RGB:{rgb}')
 
-def draw_repeat(length_diff, heading_diff, red_diff, green_diff, blue_diff):
+def draw_repeat_mode(length_diff, heading_diff, red_diff, green_diff, blue_diff):
     length.set(length.get() + length_diff)
     heading.set((heading.get() + heading_diff) % 360)
 
@@ -26,7 +26,7 @@ def draw_repeat(length_diff, heading_diff, red_diff, green_diff, blue_diff):
     newb = min(blue.get() + blue_diff, 255)
     blue.set(max(newb, 0))
 
-    draw_manual()
+    draw_manual_mode()
 
 def tab_manual_mode(parent):
     frame = ttk.Frame(parent)
@@ -62,7 +62,7 @@ def tab_manual_mode(parent):
     blue_sb.pack()
 
     def on_draw_click():
-        draw_manual()
+        draw_manual_mode()
 
     draw_btn = ttk.Button(frame, text='Рисовать', command=on_draw_click)
     draw_btn.pack(pady=5)
@@ -74,33 +74,33 @@ def tab_repeat_mode(parent):
 
     label = ttk.Label(frame, text='Изменение длины')
     label.pack()
-    length_sb = ttk.Spinbox(frame, from_=-100, to=100, increment=1)
-    length_sb.set(0)
-    length_sb.pack()
+    length_diff_sb = ttk.Spinbox(frame, from_=-100, to=100, increment=1)
+    length_diff_sb.set(0)
+    length_diff_sb.pack()
 
     label = ttk.Label(frame, text='Изменение поворота')
     label.pack()
-    heading_sb = ttk.Spinbox(frame, from_=-360, to=360, increment=5)
-    heading_sb.set(0)
-    heading_sb.pack()
+    heading_diff_sb = ttk.Spinbox(frame, from_=-360, to=360, increment=5)
+    heading_diff_sb.set(0)
+    heading_diff_sb.pack()
 
     label = ttk.Label(frame, text='Изменение красного')
     label.pack()
-    red_sb = ttk.Spinbox(frame, from_=-255, to=255, increment=1)
-    red_sb.set(0)
-    red_sb.pack()
+    red_diff_sb = ttk.Spinbox(frame, from_=-255, to=255, increment=1)
+    red_diff_sb.set(0)
+    red_diff_sb.pack()
 
     label = ttk.Label(frame, text='Изменение зеленого')
     label.pack()
-    green_sb = ttk.Spinbox(frame, from_=-255, to=255, increment=1)
-    green_sb.set(0)
-    green_sb.pack()
+    green_diff_sb = ttk.Spinbox(frame, from_=-255, to=255, increment=1)
+    green_diff_sb.set(0)
+    green_diff_sb.pack()
 
     label = ttk.Label(frame, text='Изменение синего')
     label.pack()
-    blue_sb = ttk.Spinbox(frame, from_=-255, to=255, increment=1)
-    blue_sb.set(0)
-    blue_sb.pack()
+    blue_diff_sb = ttk.Spinbox(frame, from_=-255, to=255, increment=1)
+    blue_diff_sb.set(0)
+    blue_diff_sb.pack()
 
     label = ttk.Label(frame, text='Повторы')
     label.pack()
@@ -110,12 +110,12 @@ def tab_repeat_mode(parent):
 
     def on_draw_click():
         for _ in range(int(repeat_sb.get())):
-            draw_repeat(
-                int(length_sb.get()),
-                int(heading_sb.get()),
-                int(red_sb.get()),
-                int(green_sb.get()),
-                int(blue_sb.get()),
+            draw_repeat_mode(
+                int(length_diff_sb.get()),
+                int(heading_diff_sb.get()),
+                int(red_diff_sb.get()),
+                int(green_diff_sb.get()),
+                int(blue_diff_sb.get()),
             )
 
     draw_btn = ttk.Button(frame, text='Рисовать', command=on_draw_click)
