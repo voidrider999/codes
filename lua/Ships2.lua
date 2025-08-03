6a6c3b75cc2ea794c2aeb0b32e2a61eb71376e20
voidrider999 <voidrider999@gmail.com>
2025-08-03 21:00:41 +0500

    Ships2.lua: another bugs fixed

--- lua/Ships2.lua
+++ lua/Ships2.lua
@@ -1,14 +1,17 @@
 ships = {}
 while true do
-    io.write("Введите название коробля: ")
+    io.write("Введите название корабля: ")
     local ship = io.read()
-    
-    if ships[ship] == nil then
+    if ship == "nil" then
+        break
+    end
+    if ships[ship] == n then
         ships[ship] = 1
+        print(ships)
     else
         ships[ship] = ships[ship] + 1
     end
     for k,v in pairs(ships) do
         print(k.. ":" ..v)
-end
+    end
 end
