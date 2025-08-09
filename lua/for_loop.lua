3807ab0d6d541ee9ae883e5774cb831632807d4d
voidrider999 <voidrider999@gmail.com>
2025-08-09 15:59:47 +0500

    Port for_loop.y to for_loop.lua

---
+++ lua/for_loop.lua
@@ -0,0 +1,13 @@
+offices = {
+    {"john", "sam"},
+    {"peter", "stu", "miles", "isaac"},
+    {"martha", "chloe", "zanthia"}
+}
+
+for _, office in ipairs(offices) do
+    io.write(#office .. ": ")
+    for _, person in ipairs(office) do
+        io.write(person .. ": ")
+    end
+    io.write("\n")
+end
